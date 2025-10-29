079e4b8b237b4f3025c8f934ab33421b5d83e967
caion <caioncaminha@gmail.com>
2025-10-29 01:03:44 -0300

    Still need to debug alot, committing to go sleep

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,51 @@
-export default function App() {
-  return <h1>Testando renderização</h1>;
+import { useEffect, useState } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import {
+  Main,
+  Timeline,
+  About,
+  Project,
+  Contact,
+  Navigation,
+  Footer,
+} from "./components";
+import Education from "./components/Education";
+import "./index.scss";
+import { LanguageProvider } from "./context/LanguageContext";
+
+function App() {
+  const [mode, setMode] = useState<string>("dark");
+
+  const handleModeChange = () => {
+    setMode((prevMode) => (prevMode === "dark" ? "light" : "dark"));
+  };
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    AOS.init({ duration: 700, once: false, easing: "ease-out-cubic" });
+  }, []);
+
+  return (
+    <LanguageProvider>
+      <div
+        className={`main-container  ${
+          mode === "dark" ? "dark-mode" : "light-mode"
+        }`}
+        data-aos="fade-up"
+      >
+        <Navigation parentToChild={{ mode }} modeChange={handleModeChange} />
+        <Main />
+        <About />
+        <Timeline />
+        <Project />
+        <Education />
+        <Contact />
+
+        <Footer />
+      </div>
+    </LanguageProvider>
+  );
 }
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App"; // Garante que importa App.tsx
+import "./index.scss"; // Importa o SCSS global
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
